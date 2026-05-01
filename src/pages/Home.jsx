dfb5d449dfd9809d353ee39134bfa2574a37bcd8
Abdul-Rehman33 <arehmanshakeel33@gmail.com>
2026-04-30 18:43:29 -0700

    add a categories

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Hero from "../components/Hero/Hero.jsx";
 import Navbar from "../components/Navbar/Navabr.jsx";
+import Categories from "../components/Categories/Categories.jsx";
 
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
         <>
           <Navbar />
           <Hero />
-         </>
+          <Categories />
+        </>
     );
 }
